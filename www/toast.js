d14eeffd5f291feca06f5b7d594e081ef5057b04
master <wwtt1984@163.com>
2013-12-31 10:37:44 +0800

    toast first

---
+++ www/toast.js
@@ -0,0 +1,32 @@
+var Toast = function () {};
+
+Toast.prototype._failureCallback = function (msg) {
+    console.log("Toast Javascript Callback Error: " + msg)
+};
+
+Toast.prototype._callNative = function (action, args, successCallback) {
+    if (arguments.length == 2) {
+        args = []
+    }
+    return cordova.exec(
+        successCallback, // called when signature capture is successful
+        this._failureCallback, // called when signature capture encounters an error
+        'ToastPlugin', // Tell cordova that we want to run "JPush"
+        action, // Tell the plugin the action we want to perform
+        args); // List of arguments to the plugin
+};
+
+
+
+/**
+ * 设置提示值
+ * @param msg
+ * @param callback
+ * @returns {*}
+ */
+Toast.prototype.ShowToast = function (msg, callback) {
+    this._callNative("Toast", [msg], callback);
+    return this;
+};
+
+module.exports = (new Toast());
