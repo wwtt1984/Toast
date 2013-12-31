e426109a49e298117c6ab72c20e5b5ad29db1adb
master <wwtt1984@163.com>
2013-12-31 13:01:13 +0800

    2013123116

--- www/toast.js
+++ www/toast.js
@@ -1,27 +1,11 @@
 ﻿var Toast = function () {};
-
-Toast.prototype._callNative = function (action, args) {
-    if (arguments.length == 2) {
-        args = []
-    }
-    return cordova.exec(
-        null, // called when signature capture is successful
-        null, // called when signature capture encounters an error
-        'ToastPlugin', // Tell cordova that we want to run "JPush"
-        action, // Tell the plugin the action we want to perform
-        args); // List of arguments to the plugin
-};
-
-
-
 /**
  * 设置提示值
- * @param msg
- * @param callback
+ * @param content
+ * @param length
  * @returns {*}
  */
 Toast.prototype.ShowToast = function (content, length) {
     return cordova.exec(null, null,"ToastPlugin","Toast",[content,length]);
 };
-
 module.exports = (new Toast());
